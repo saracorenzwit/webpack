f85f287f048e28ef388d7f0464a6037e47bff025
Sara Corenzwit <scorenwitz@hugeinc.com>
2017-01-13 22:05:12 -0500

    initial commit

---
+++ howdoihelpus/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <homepage></homepage>
+  </div>
+</template>
+
+<script>
+import Homepage from './components/Homepage'
+
+export default {
+  name: 'app',
+  components: {
+    Homepage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Work Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
